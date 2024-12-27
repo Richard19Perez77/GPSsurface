5e8af5e7d7d4a9f44d6fcb63f4be181a01ca69bd
richa <richard19.perez77@yahoo.com>
2024-12-27 17:07:38 +0700

    revert changes

--- app/src/main/java/com/rperez/gpssurface/MainActivity.kt
+++ app/src/main/java/com/rperez/gpssurface/MainActivity.kt
@@ -96,31 +96,33 @@ class PathViewModel : ViewModel() {
 
 class PointsViewModel : ViewModel() {
 
-    val screenPoints = mutableListOf<Pair<Double, Double>>()
-
     var h = 0.0f
     var w = 0.0f
+
+    val randomPoints = List(10) {
+        Pair(
+            (-90..90).random().toDouble(), (-180..180).random().toDouble()
+        )
+    }
+
+    val screenPoints = mutableListOf<Pair<Double, Double>>()
 
     fun setHW(h: Float, w: Float) {
         this.h = h
         this.w = w
     }
 
-    fun generateScreenPoints() {
-        screenPoints.clear()
-        List(10) {
-            Pair(
-                (-90..90).random().toDouble(), (-180..180).random().toDouble()
-            )
-        }.forEach {
-            screenPoints.add(getScreenXY(it.first, it.second))
-        }
-    }
-
     fun getScreenXY(latitude: Double, longitude: Double): Pair<Double, Double> {
         val x = ((longitude + 180) / 360) * w
         val y = ((90 - latitude) / 180) * h
         return Pair(x, y)
+    }
+
+    fun generateScreenPoints() {
+        screenPoints.clear()
+        randomPoints.forEach {
+            screenPoints.add(getScreenXY(it.first, it.second))
+        }
     }
 }
 
