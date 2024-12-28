a303694b3cd6f25d39c0f86c2a94dc527bcc6fe2
richa <richard19.perez77@yahoo.com>
2024-12-28 13:05:00 +0700

    remove unused import

--- app/src/main/java/com/rperez/gpssurface/MainActivity.kt
+++ app/src/main/java/com/rperez/gpssurface/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
