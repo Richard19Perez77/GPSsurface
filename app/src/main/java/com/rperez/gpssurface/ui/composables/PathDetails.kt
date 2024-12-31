43badcb261ff7c7f53621a23a73553c4b30051f5
richa <richard19.perez77@yahoo.com>
2024-12-31 22:11:10 +0700

    comments added

--- app/src/main/java/com/rperez/gpssurface/ui/composables/PathDetails.kt
+++ app/src/main/java/com/rperez/gpssurface/ui/composables/PathDetails.kt
@@ -10,11 +10,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+/**
+ * A composable function that displays the details of a calculated path.
+ *
+ * @param resultPath A MutableState containing a Pair, where:
+ * - The first value is a String representing the path.
+ * - The second value is an Int representing the calculated distance.
+ */
 @Composable
 fun PathDetails(
     resultPath: MutableState<Pair<String, Int>>
 ) {
-
     var pathResult = remember { resultPath }
 
     Column(
